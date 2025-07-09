ffde1f5bf85a5dbf7008c9daaaf318fa36b0300e
Deyan Ginev <deyan.ginev@gmail.com>
2025-07-08 21:21:51 -0400

    ruff only touchups

--- conversion-container/conversion/domain/conversion.py
+++ conversion-container/conversion/domain/conversion.py
@@ -1,38 +1,42 @@
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import List, Union
-from abc import ABC, abstractmethod
-from pydantic import BaseModel
 
 from arxiv.identifier import Identifier
 
+
 @dataclass
-class ConversionPayload (ABC):
+class ConversionPayload(ABC):
     identifier: Union[Identifier, int]
     single_file: bool
 
     @property
     @abstractmethod
-    def name (self) -> str:
-        ...
+    def name(self) -> str: ...
+
 
 @dataclass
-class SubmissionConversionPayload (ConversionPayload):
+class SubmissionConversionPayload(ConversionPayload):
     identifier: int
 
     @property
-    def name (self) -> str:
+    def name(self) -> str:
         return str(self.identifier)
 
+
 @dataclass
-class DocumentConversionPayload (ConversionPayload):
+class DocumentConversionPayload(ConversionPayload):
     identifier: Identifier
     is_latest: bool
 
     @property
-    def name (self) -> str:
-        return self.identifier.idv if \
-            not self.identifier.is_old_id else \
-            f'{self.identifier.filename}v{self.identifier.version}'
+    def name(self) -> str:
+        return (
+            self.identifier.idv
+            if not self.identifier.is_old_id
+            else f"{self.identifier.filename}v{self.identifier.version}"
+        )
+
 
 @dataclass
 class LaTeXMLOutput:
--- conversion-container/conversion/services/files/main_src.py
+++ conversion-container/conversion/services/files/main_src.py
@@ -3,6 +3,7 @@ import re
 
 from ...exceptions import MainTeXError
 
+
 def find_main_tex_source(path: str) -> str:
     """
     Looks inside the directory at "path" and determines the
@@ -24,7 +25,7 @@ def find_main_tex_source(path: str) -> str:
         File path to the main .tex source in the directory
     """
     try:
-        tex_files = [f for f in os.listdir(path) if f.endswith('.tex')]
+        tex_files = [f for f in os.listdir(path) if f.endswith(".tex")]
         if len(tex_files) == 1:
             return os.path.join(path, tex_files[0])
             # Returns the only .tex file in the source files
@@ -42,10 +43,9 @@ def find_main_tex_source(path: str) -> str:
                                 main_files[tf] = 0
                             break
             if len(main_files) == 1:
-                return (os.path.join(path, list(main_files)[0]))
+                return os.path.join(path, list(main_files)[0])
             elif len(main_files) == 0:
-                raise MainTeXError(
-                    f"No main .tex found file in {path}")
+                raise MainTeXError(f"No main .tex found file in {path}")
             else:
                 # account for the two main ways of creating multi-file
                 # submissions on overleaf (standalone, subfiles)
@@ -59,5 +59,4 @@ def find_main_tex_source(path: str) -> str:
                                 # #the main file is just {article} or something else
                 return max(main_files, key=main_files.__getitem__)
     except Exception as exc:
-        raise MainTeXError(
-            f"Process to find main .tex file in {path} failed") from exc
+        raise MainTeXError(f"Process to find main .tex file in {path} failed") from exc
