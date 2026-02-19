064b1ac446dc223da8f831b888e8724e211e3ce8
Deyan Ginev <deyan.ginev@gmail.com>
2026-02-19 09:17:41 -0500

    [HTML25-194] reorganize HTML Article scaffold

--- SubmitReverseProxy/entry_point.py
+++ SubmitReverseProxy/entry_point.py
@@ -1,6 +1,12 @@
+import sys
+from ReverseProxy.scaffold_response import browse_urls_fallback
 from ReverseProxy.factory import create_web_app
+import ReverseProxy.files
+sys.modules["arxiv.files"] = ReverseProxy.files
 
 app = create_web_app()
+
+app.url_build_error_handlers.append(browse_urls_fallback)
 
 if __name__=='__main__':
     app.run(debug=False)
